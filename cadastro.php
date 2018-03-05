7d83cb5f8b78f56836e947385daff43d4d776ff7
Ricardo Crivelli <ricardocrivelli@gmail.com>
2018-03-05 13:32:45 -0300

    cadastro pelo combo

--- cadastro.php
+++ cadastro.php
@@ -1,98 +1,193 @@
 <?php
-    include 'conn.php';
-    include 'header.php';
+include 'conn.php';
+include 'header.php';
 ?>
 
 <form method="post" action="cadastro_gravar.php">
-    <div id="panel" class="container" style="width: 50%" >
-         <label>CURSO:</label>
-            <?php
-            $result2 = $conn->query("SELECT idturma, descricao FROM TURMAS;");
+    <div id="panel" class="container" style="width: 50%">
+        <label>CURSO:</label>
+		<?php
+		$result2 = $conn->query( "SELECT idturma, descricao FROM TURMAS;" );
 
-            echo "<select id='courses' name='courses' class=\"form-control\"> ";
-            while ($row = $result2->fetch_assoc())
-            {
-                unset($id, $name);
-                $id = $row['idturma'];
-                $descricao = $row['descricao'];
-                echo '<option value="' . $id . '">' . $descricao . '</option>';
-            }
-            echo "</select>";
-            ?>
+		echo "<select id='courses' name='courses' class=\"form-control\"> ";
+		while ( $row = $result2->fetch_assoc() ) {
+			unset( $id, $name );
+			$id        = $row['idturma'];
+			$descricao = $row['descricao'];
+			echo '<option value="' . $id . '">' . $descricao . '</option>';
+		}
+		echo "</select>";
+		?>
     </div>
 
-    <div id="panel2" class="container" style="width: 50%" >
+    <div id="panel2" class="container" style="width: 50%">
         <label>DISCIPLINA:</label>
-            <?php
-            $result2 = $conn->query("SELECT iddisciplina, sigla, descricao FROM DISCIPLINAS ORDER BY sigla;");
+		<?php
+		$result2 = $conn->query( "SELECT iddisciplina, sigla, descricao FROM DISCIPLINAS ORDER BY sigla;" );
 
-            echo "<select id='disciplinas' name='disciplinas' class=\"form-control\" > ";
-            while ($row2 = $result2->fetch_assoc())
-            {
-                unset($id2, $name2);
-                $id2 = $row2['iddisciplina'];
-                $sigla= $row2['sigla'];
-                $descricao2= $row2['descricao'];
-                echo '<option value="' . $id2 . '">'. $sigla . " - "  . $descricao2 . '</option>';
-            }
-            echo "</select>";
-            ?>
+		echo "<select id='disciplinas' name='disciplinas' class=\"form-control\" > ";
+		while ( $row2 = $result2->fetch_assoc() ) {
+			unset( $id2, $name2 );
+			$id2        = $row2['iddisciplina'];
+			$sigla      = $row2['sigla'];
+			$descricao2 = $row2['descricao'];
+			echo '<option value="' . $id2 . '">' . $sigla . " - " . $descricao2 . '</option>';
+		}
+		echo "</select>";
+		?>
     </div>
 
-    <div id="panel3" class="container" style="width: 50%" >
+    <div id="panel3" class="container" style="width: 50%">
         <label>PROFESSOR:</label>
-            <?php
-            $result2 = $conn->query("SELECT * FROM PROFESSORES ORDER BY nome;");
+		<?php
+		$result2 = $conn->query( "SELECT * FROM PROFESSORES ORDER BY nome;" );
 
-            echo "<select id='professores' name='professores' class=\"form-control\" > ";
-            while ($row2 = $result2->fetch_assoc())
-            {
-                unset($id2, $name2);
-                $id2 = $row2['idprofessor'];
-                $sigla= $row2['nome'];
-                echo '<option value="' . $id2 . '">'. $sigla . '</option>';
-            }
-            echo "</select>";
-            ?>
+		echo "<select id='professores' name='professores' class=\"form-control\" > ";
+		while ( $row2 = $result2->fetch_assoc() ) {
+			unset( $id2, $name2 );
+			$id2   = $row2['idprofessor'];
+			$sigla = $row2['nome'];
+			echo '<option value="' . $id2 . '">' . $sigla . '</option>';
+		}
+		echo "</select>";
+		?>
     </div>
-
-    <div id="panel4" class="container" style="width: 50%" >
+    <!--
+    <div id="panel4" class="container" style="width: 50%">
         <label>HORÁRIO:</label>
-            <?php
-            $result2 = $conn->query("SELECT * FROM HORARIOS ORDER BY INICIO;");
+		<?php
+		$result2 = $conn->query( "SELECT * FROM HORARIOS ORDER BY INICIO;" );
 
-            echo "<select id='horarios' name='horarios' class=\"form-control\" > ";
-            while ($row2 = $result2->fetch_assoc())
-            {
-                unset($id2, $name2);
-                $id2 = $row2['idhorario'];
-                echo '<option value="' . $id2 . '">'. $row2['periodo'] . " - " . $row2['descricao'] . " - " . $row2['inicio'] ." às ". $row2['fim'] . '</option>';
-            }
-            echo "</select>";
-            ?>
+		echo "<select id='horarios' name='horarios' class=\"form-control\" > ";
+		while ( $row2 = $result2->fetch_assoc() ) {
+			unset( $id2, $name2 );
+			$id2 = $row2['idhorario'];
+			echo '<option value="' . $id2 . '">' . $row2['periodo'] . " - " . $row2['descricao'] . " - " . $row2['inicio'] . " às " . $row2['fim'] . '</option>';
+		}
+		echo "</select>";
+		?>
     </div>
 
-    <div id="panel5" class="container" style="width: 50%" >
+    <div id="panel5" class="container" style="width: 50%">
         <label>DIA:</label>
-            <?php
-            echo "<select id='dias' name='dias' class=\"form-control\" > ";
-            $k = 0;
-            while ($k < 6)
-            {
-                echo '<option value="' . $k . '">'. DIAS[$k] . '</option>';
-                $k++;
-            }
-            echo "</select>";
-            ?>
+		<?php
+		echo "<select id='dias' name='dias' class=\"form-control\" > ";
+		$k = 0;
+		while ( $k < 6 ) {
+			echo '<option value="' . $k . '">' . DIAS[ $k ] . '</option>';
+			$k ++;
+		}
+		echo "</select>";
+		?>
+    </div>
+    -->
+
+    <div class="container" style="width: 50%; margin-top: 30px;">
+        <label>MANHÃ:</label>
+
+        <table class="table table-striped" style="text-align: center">
+            <thead>
+            <tr>
+                <th><!-- VAZIO --></th>
+				<?php
+				// CABECALHO
+				foreach ( DIAS as $dia ) {
+					echo '<th>' . $dia . '</th>';
+				}
+				?>
+            </tr>
+            </thead>
+            <tbody>
+				<?php
+
+				$rs = $conn->query( "SELECT * FROM horarios WHERE periodo='MANHA'" );
+				while($h = $rs->fetch_object()) {
+				    echo '<tr>';
+				    echo '<th>' . $h->descricao .' (' . $h->inicio . ' - ' . $h->fim .')</th>';
+					for($i=0; $i<6; $i++) {
+					    echo '<td><input type="checkbox" name="manha[]" value="'.$h->idhorario.'_'.$i.'"/></td>';
+                    }
+                    echo '</th>';
+                }
+
+				?>
+            </tbody>
+        </table>
+    </div>
+
+    <div class="container" style="width: 50%; margin-top: 30px;">
+        <label>TARDE:</label>
+
+        <table class="table table-striped" style="text-align: center">
+            <thead>
+            <tr>
+                <th><!-- VAZIO --></th>
+				<?php
+				// CABECALHO
+				foreach ( DIAS as $dia ) {
+					echo '<th>' . $dia . '</th>';
+				}
+				?>
+            </tr>
+            </thead>
+            <tbody>
+			<?php
+
+			$rs = $conn->query( "SELECT * FROM horarios WHERE periodo='TARDE'" );
+			while($h = $rs->fetch_object()) {
+				echo '<tr>';
+				echo '<th>' . $h->descricao .' (' . $h->inicio . ' - ' . $h->fim .')</th>';
+				for($i=0; $i<6; $i++) {
+					echo '<td><input type="checkbox" name="tarde[]" value="'.$h->idhorario.'_'.$i.'" /></td>';
+				}
+				echo '</th>';
+			}
+
+			?>
+            </tbody>
+        </table>
+    </div>
+
+    <div class="container" style="width: 50%; margin-top: 30px;">
+        <label>NOITE:</label>
+
+        <table class="table table-striped" style="text-align: center">
+            <thead>
+            <tr>
+                <th><!-- VAZIO --></th>
+				<?php
+				// CABECALHO
+				foreach ( DIAS as $dia ) {
+					echo '<th>' . $dia . '</th>';
+				}
+				?>
+            </tr>
+            </thead>
+            <tbody>
+			<?php
+
+			$rs = $conn->query( "SELECT * FROM horarios WHERE periodo='NOITE'" );
+			while($h = $rs->fetch_object()) {
+				echo '<tr>';
+				echo '<th>' . $h->descricao .' (' . $h->inicio . ' - ' . $h->fim .')</th>';
+				for($i=0; $i<6; $i++) {
+					echo '<td><input type="checkbox" name="noite[]" value="'.$h->idhorario.'_'.$i.'"" /></td>';
+				}
+				echo '</th>';
+			}
+
+			?>
+            </tbody>
+        </table>
     </div>
 
     <div style="text-align: center; margin-top: 50px;">
-       <input type="submit"  class="btn btn-success btn-large" value="SEND TO BACK!" id="cadastrar" name="cadastrar">
+        <input type="submit" class="btn btn-success btn-large" value="SEND TO BACK!" id="cadastrar" name="cadastrar">
     </div>
 
 </form>
 
 
-<?php include 'footer.php' ; ?>
+<?php include 'footer.php'; ?>
 
 
--- cadastro_gravar.php
+++ cadastro_gravar.php
@@ -10,9 +10,10 @@ include 'conn.php';
 $curso = $_POST['courses'];
 $disciplina = $_POST['disciplinas'];
 $professor = $_POST['professores'];
-$horario = $_POST['horarios'];
-$dia = $_POST['dias'];
+//$horario = $_POST['horarios'];
+//$dia = $_POST['dias'];
 
+/*
 $sql = "INSERT INTO HORARIOS_DISCIPLINAS (iddisciplina, idturma, idhorario, idprofessor, diasemana) ".
        "VALUES (". $disciplina. "," . $curso . "," . $horario ."," . $professor ."," . $dia.")";
 
@@ -23,4 +24,31 @@ if (mysqli_query($conn, $sql))
 else
 {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}*/
+
+foreach ($_POST['manha'] as $m) {
+	$val = explode("_", $m);
+	$sql = "INSERT INTO HORARIOS_DISCIPLINAS (iddisciplina, idturma, idhorario, idprofessor, diasemana) ".
+	       "VALUES (". $disciplina. "," . $curso . "," . $val[0] ."," . $professor ."," . $val[1].")";
+	mysqli_query($conn, $sql);
 }
+
+foreach ($_POST['tarde'] as $m) {
+	$val = explode("_", $m);
+	$sql = "INSERT INTO HORARIOS_DISCIPLINAS (iddisciplina, idturma, idhorario, idprofessor, diasemana) ".
+	       "VALUES (". $disciplina. "," . $curso . "," . $val[0] ."," . $professor ."," . $val[1].")";
+	mysqli_query($conn, $sql);
+}
+
+foreach ($_POST['noite'] as $m) {
+	$val = explode("_", $m);
+	$sql = "INSERT INTO HORARIOS_DISCIPLINAS (iddisciplina, idturma, idhorario, idprofessor, diasemana) ".
+	       "VALUES (". $disciplina. "," . $curso . "," . $val[0] ."," . $professor ."," . $val[1].")";
+	mysqli_query($conn, $sql);
+}
+
+?>
+<script>
+	alert('Registro gravado com sucesso!');
+	window.location.href = 'cadastro.php';
+</script>
